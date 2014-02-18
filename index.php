16bf2422ae1d3e251d52850fbb06f53db323c5cb
Alex Recker <alex@reckerfamily.com>
2014-02-18 14:01:36 -0600

    Update index.php

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ function searchIt()
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   ga('create', 'UA-42540208-5', 'bobrosssearch.com');
   ga('send', 'pageview');
 
