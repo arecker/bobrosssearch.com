f88316f5590e66f4460e35a65abe9c16b3eca361
Alex Recker <alex@reckerfamily.com>
2014-02-18 14:39:38 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 	<meta charset="UTF-8">
         <title>Bob Ross Search</title>
-        <link rel="icon" type="image/gif" href="img/favicon.gif"> <!-- Favicon -->
+        <link rel="icon" type="image/gif" href="img/favicon.gif" media="screen"/> <!-- Favicon -->
         <meta name="description" content="Begin each Internet query with wisdom from Bob Ross.">
 </head>
 
@@ -35,8 +35,8 @@
 </body>
 
 <footer>
-<link rel="stylesheet" href="lib/css/bootstrap.min.css"> <!-- Bootstrap -->
-        <link rel="stylesheet" href="lib/css/font-awesome.min.css"> <!-- Font Awesome -->
+<link rel="stylesheet" href="lib/css/bootstrap.min.css" media="screen" /> <!-- Bootstrap -->
+        <link rel="stylesheet" href="lib/css/font-awesome.min.css" media="screen" /> <!-- Font Awesome -->
        
         <script type="text/javascript" src="lib/js/jquery.min.js"></script> <!-- Jquery -->
 
@@ -114,17 +114,6 @@ function searchIt()
 </script>
 
 
-
-<!--script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  ga('create', 'UA-42540208-5', 'bobrosssearch.com');
-  ga('send', 'pageview');
-
-</script-->
 
 <script type="text/javascript">
 var _gaq = _gaq || [];
