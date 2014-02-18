2e236fb19731cc6f1ed56e6076d6f5b3a4494deb
Alex Recker <alex@reckerfamily.com>
2014-02-18 14:03:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -115,7 +115,7 @@ function searchIt()
 
 
 
-<script>
+<!--script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -124,5 +124,18 @@ function searchIt()
   ga('create', 'UA-42540208-5', 'bobrosssearch.com');
   ga('send', 'pageview');
 
+</script-->
+
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-42540208-5']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; 
+
+ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 </script>
 </footer>
